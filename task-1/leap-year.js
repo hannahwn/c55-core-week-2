d7f08e6b371bd3a9ebb3f463b8a1a4f483db2990
hannahwn <hannahwanjikunyongo@gmail.com>
2026-01-21 00:16:23 +0100

    working so far

--- task-1/leap-year.js
+++ task-1/leap-year.js
@@ -7,3 +7,11 @@ const prompt = promptSync();
 // Step 1: prompt the user to enter a year
 // Step 2: convert the user input to a number so we can perform calculations
 // Step 3: Implement the logic
+let year = prompt('Enter an year: ');
+
+if (year % 4 == 0){
+    console.log('Yes, '+year + ' is a leap year');
+    }
+   else if(year % 100 == 0){
+        console.log('No, '+year+' is not a leap year'); 
+}
