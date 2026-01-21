0ab9c5d1523f6b7a6fffcd3be2b4ceb0f337bed0
hannahwn <hannahwanjikunyongo@gmail.com>
2026-01-21 10:52:41 +0100

    added 2 valid users

--- task-2/login.js
+++ task-2/login.js
@@ -4,9 +4,15 @@ import { errorMessage, successMessage } from './app.js';
 let incorrectAttempts = 0;
 
 function onLogin(username, password) {
+  
   // Write your code here.
   // Use the variables 'username' and 'password' to access the input values
   // Use incorrectAttempts to track the number of failed attempts
+
+  const validUsers =[
+    {username : 'admin', password : 'Hack1234'},
+    {username : 'user', password : 7654321}
+  ];
 }
 
 // Do not change the line below
